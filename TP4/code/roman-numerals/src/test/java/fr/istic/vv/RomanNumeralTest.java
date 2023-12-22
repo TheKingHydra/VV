261f0d3a2ce61fb2769535a38c92b131bee48bc5
Hywel <95933117+TheKingHydra@users.noreply.github.com>
2023-12-22 14:07:53 +0100

    Delete TP4 directory

--- TP4/code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++
@@ -1,9 +0,0 @@
-package fr.istic.vv;
-import net.jqwik.api.*;
-
-public class RomanNumeralTest {
-    @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
-    }
-}
