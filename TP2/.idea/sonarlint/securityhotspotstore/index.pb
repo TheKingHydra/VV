2c4313fece53de980a598af16036346a019eba05
TheKingHydra <95933117+TheKingHydra@users.noreply.github.com>
2023-12-22 14:07:08 +0100

    No getter finished

--- TP2/.idea/sonarlint/securityhotspotstore/index.pb
+++ TP2/.idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,12 @@ k
 ;code/javaparser-starter/src/main/java/fr/istic/vv/Main.java,b\b\bb43b2dad99cfac9cc8b63e2fd48a2f29f9e1f52
 J
 code/Exercise4/Person.java,e\2\e27e8e34e5230562777115c1026306496d0604ad
+;
+reponse.txt,c\d\cd1ccbb10fb9e319c605c8e52172f06d5e1788b1
+:
+
+report.txt,4\9\49fb9acae7ad9192b406557cfddd733e952abc93
+K
+code/Exercise4/Person2.java,0\d\0dec2553a43b1b84815d28b4874c85f7379f2a4a
+H
+code/Exercise4/README.md,e\1\e1c331d9ea5b64ca5509a81ecf5a40b0a614dbad
--- TP2/code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ TP2/code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -4,24 +4,32 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import javax.xml.transform.stream.StreamSource;
+import java.sql.SQLOutput;
+import java.util.*;
 
 
 // This class visits a compilation unit and
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
-    public HashSet<String> attributes;
 
 
-    public HashSet<String> getters;
-    public String className;
+    public int indexClass=-1;
+    public int indexPackage =-1;
+
+    private String fileName="";
+
+    public final List<String> packageName; //liste des noms de package
+    public final List<List<HashSet<String>>> tabAttributes; //liste des noms d'attributs (package/class/attributs)
+    public final List<List<HashSet<String>>> tabGetters; //liste des noms de getters (package/class/getters)
+    public final List<List<String>> tabClassName;   //liste des classNames (package/class)
 
     public PublicElementsPrinter(){
-        attributes= new HashSet<>();
-        getters= new HashSet<>();
-        className="";
+        tabAttributes= new ArrayList<>(); //Init la liste (package)
+        tabGetters= new ArrayList<>();  //Init la liste (package)
+        tabClassName=new ArrayList<>(); //Init la liste (package)
+        packageName=new ArrayList<>(); //Init la liste de package
     }
     @Override
     public void visit(CompilationUnit unit, Void arg) {
@@ -31,16 +39,22 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     }
 
     /**
-     * Print pour les types (class par exemple)
+     * Visiteur pour les méthodes
      * @param declaration
      * @param arg
      */
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
-        if(!declaration.isPublic()) {System.out.println("Je suis privé");return;}
-        System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
-        for (int i = 0; i < declaration.getFields().size();i++){
-            attributes.add(declaration.getFields().get(i).toString().substring(0,declaration.getFields().get(i).toString().indexOf(";")));
+        if(!declaration.isPublic()) {
+            System.out.println("Privé !");
+            return;
         }
+        HashSet<String> attributes = new HashSet<>();  //Creation of an attributes set
+        for (int i = 0; i < declaration.getFields().size();i++){
+            if (declaration.getFields().get(i).isPrivate()){ //Checks if the attribute is private
+                attributes.add(getNameOfAttributes(declaration.getFields().get(i).toString()));
+            }
+        }
+        tabAttributes.get(indexPackage).set(indexClass,attributes); //Set
         for(MethodDeclaration method : declaration.getMethods()) {
             method.accept(this, arg);
         }
@@ -51,13 +65,62 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
         }
     }
 
+    /**
+     * Visiteur pour les classes ou interfaces
+     * @param declaration
+     * @param arg
+     */
     @Override
     public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
-        System.out.println("Class or Interface");
-        className=declaration.toString();
-        visitTypeDeclaration(declaration, arg);
+        String packageOfDeclaration = getPackageName(declaration.getFullyQualifiedName().toString());   //Get the packageName of the declaration
+        if (packageName.isEmpty() || !(packageName.get(indexPackage).equals(packageOfDeclaration+" : "+fileName))) {    //if the set of packageName is empty (at start), add the packageName to the set at index 0.
+            indexPackage++;
+            packageName.add(indexPackage,packageOfDeclaration+" : "+fileName);    //Add the new package to the package list
+            tabClassName.add(indexPackage,new ArrayList<>());  //Init a new list at index (indexPackage) (package/class) to get classes of package
+            tabGetters.add(indexPackage, new ArrayList<>()); //Init a new list at index (indexPackage) (package/class/getters) to add all getters from package
+            tabAttributes.add(indexPackage, new ArrayList<>()); //Init a new list at index (indexPackage) (package/class/getters) to add all attributes from package
+            indexClass=-1;
+        }
+        //WE HAVE TO CHECK THE NAME OF THE CLASS (IF IT IS DIFFERENT OR NOT)
+        String classOfDeclaration = getNameOfClass(declaration.toString());
+
+        if (tabClassName.get(indexPackage).isEmpty()){                                              //If the classlist is empty (at start of new package)
+            tabClassName.add(indexPackage,new ArrayList<>());                                       //create a new arrayList
+        } else if (!(tabClassName.get(indexPackage).get(indexClass).equals(classOfDeclaration))){   //If the class of Declaration is different from the last class saved, add the new class to the list
+
+        }
+        indexClass++;
+        tabClassName.get(indexPackage).add(indexClass, classOfDeclaration);                         //add the class of Declaration to the set
+        tabAttributes.get(indexPackage).add(indexClass, new HashSet<>());                           //
+        tabGetters.get(indexPackage).add(indexClass, new HashSet<>());                              //
+        tabClassName.get(indexPackage).add(getNameOfClass(declaration.toString()));                 //Add the name of class of Declaration to the array of classNames.
+
+        visitTypeDeclaration(declaration, arg);                                                     //
     }
 
+    public String getPackageName(String declaration){
+        return declaration.substring(declaration.indexOf("[")+1,declaration.indexOf("."));
+    }
+    public String getNameOfMethods(String declaration){
+        int startIndex = declaration.indexOf(" ")+1;
+        int endIndex = declaration.indexOf("(",startIndex);
+        return declaration.substring(startIndex,endIndex);
+    }
+
+    public String getNameOfAttributes(String declaration){
+        int classIndex=declaration.indexOf(" ");
+        int typeIndex = classIndex+1;
+        int startIndex = declaration.indexOf(" ",typeIndex)+1;
+        int endIndex = declaration.indexOf(";",startIndex);
+        return declaration.substring(startIndex,endIndex);
+    }
+
+    public String getNameOfClass(String declaration){
+        int classIndex=declaration.indexOf("class");
+        int nameIndex = classIndex+6;
+        int endIndex = declaration.indexOf(" ",nameIndex);
+        return declaration.substring(nameIndex,endIndex);
+    }
     @Override
     public void visit(EnumDeclaration declaration, Void arg) {
         System.out.println("Enumeration");
@@ -71,26 +134,43 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
      */
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        if(!declaration.isPublic()) {System.out.println("Je suis privé");return;}
+        if(!declaration.isPublic()) {System.out.println("Une méthode priveé");return;}
         String nameOfMethod = declaration.getDeclarationAsString(false,false);
         int positionOfSpace = nameOfMethod.indexOf(" ");
         if (nameOfMethod.substring(positionOfSpace+1,positionOfSpace+4).equals("get")){
-            System.out.println("  //Je suis un getter !");
-            getters.add(declaration.getDeclarationAsString());
+            //System.out.println("  //Je suis un getter !");
+
+            tabGetters.get(indexPackage).get(indexClass).add((getNameOfMethods(declaration.getDeclarationAsString(false,false))));
         }
-        System.out.println("  " + declaration.getDeclarationAsString(true, true));
     }
 
     public boolean isThereAFieldWithoutGetter (){
-        if (getters.size()<attributes.size()) {System.out.println("Il y a moins de getters que d'attributs pour la classe "+className);return false;}
-        for (String s : attributes){
-            String firstLetter = ""+s.charAt(0);
-            if (!getters.contains("get"+firstLetter.toUpperCase()+s.substring(1))){
-                System.out.println("L'attribut "+s+" n'a pas de getter");
-                return false;
-            }
+        if (tabGetters.get(indexPackage).get(indexClass).size()<tabAttributes.get(indexPackage).get(indexClass).size()) {
+            System.out.println("Il y a moins de getters que d'attributs pour la classe " + tabClassName.get(indexPackage).get(indexClass));
+            return false;
         }
         return true;
     }
 
+    public String getFieldsWithoutGetter(){
+        String print = "";
+
+        for (int j = 0; j < tabAttributes.size();j++) {
+            print+=(packageName.get(j)+"\n");
+            for (int i = 0; i < tabAttributes.get(j).size(); i++) {
+                print += ("  " + tabClassName.get(j).get(i));
+                for (String s : tabAttributes.get(j).get(i)) {
+                    String firstLetter = "" + s.charAt(0);
+                    if (!tabGetters.get(j).get(i).contains("get" + firstLetter.toUpperCase() + s.substring(1))) {
+                        print += ("\n    L'attribut " + s + " n'a pas de getter\n\n");
+                    }
+                }
+            }
+        }
+        return print.substring(0, print.length()-2);
+    }
+
+    public void setFileName(String s){
+        this.fileName = s;
+    }
 }
